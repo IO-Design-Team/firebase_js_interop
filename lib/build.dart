910de545f9206dee8e281547600e21e0e64a3d70
Rexios <rexios@rexios.dev>
2024-08-25 14:07:40 -0400

    Use minified preamble if minify is enabled

--- lib/build.dart
+++ lib/build.dart
@@ -2,11 +2,39 @@ import 'dart:io';
 
 import 'package:node_preamble/preamble.dart';
 
+/// The optimization level for the dart2js compiler
+enum OptimizationLevel {
+  /// No optimizations (only meant for debugging the compiler).
+  // ignore: constant_identifier_names
+  O0,
+
+  /// Default (includes whole program analyses and inlining).
+  // ignore: constant_identifier_names
+  O1,
+
+  /// Safe production-oriented optimizations (like minification).
+  // ignore: constant_identifier_names
+  O2,
+
+  /// Potentially unsafe optimizations (see -h -v for details).
+  // ignore: constant_identifier_names
+  O3,
+
+  /// More agressive unsafe optimizations (see -h -v for details).
+  // ignore: constant_identifier_names
+  O4;
+
+  @override
+  String toString() => name;
+}
+
 /// A pre-built function to convert dart code into Functions-compatible JS
 ///
 /// A dart file named `src/index.dart` is compiled to `lib/index.js`
 /// which will be used by the Functions runtime
-Future<void> build({String optimization = 'O2'}) async {
+Future<void> build({
+  OptimizationLevel optimization = OptimizationLevel.O2,
+}) async {
   final build = await Process.start(
     'dart',
     [
@@ -25,7 +53,8 @@ Future<void> build({String optimization = 'O2'}) async {
     throw Exception('Build failed with exit code $exitCode');
   }
 
-  final preamble = getPreamble();
+  final preamble =
+      getPreamble(minified: optimization.index >= OptimizationLevel.O2.index);
 
   final out = File('lib/index.js');
   final content = out.readAsStringSync();
