faaafbdd9ae0a45217f5588cc7a8c48e06220d7e
Rexios <rexios@rexios.dev>
2024-08-25 17:30:16 -0400

    Add convenience method to convert Map<String, dynamic> to JS

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -5,3 +5,9 @@ extension FirebaseJsInteropJSObjectExtension on JSObject {
   /// Convert a [JSObject] to a [Map<String, dynamic>]
   Map<String, dynamic> toJson() => (dartify() as Map).cast<String, dynamic>();
 }
+
+/// Adds convenience methods to [Map<String, dynamic>]
+extension FirebaseJsInteropMapExtension on Map<String, dynamic> {
+  /// Convert a [Map<String, dynamic>] to a [JSObject]
+  JSObject get toJS => jsify() as JSObject;
+}
