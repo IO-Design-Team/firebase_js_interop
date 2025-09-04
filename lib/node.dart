3cc71c13ec863866c7771a10596ebe87cd77a7bf
Rexios <rexios@rexios.dev>
2025-09-04 13:24:55 -0400

    Add `node:fs` support

--- lib/node.dart
+++ lib/node.dart
@@ -26,3 +26,25 @@ extension type Process._(JSObject _) implements JSObject {
 /// Access to the node process object
 @JS()
 external Process get process;
+
+/// Access to Node components
+abstract final class Node {
+  /// Access to file system methods
+  static NodeFs get fs => require('node:fs') as NodeFs;
+}
+
+/// Node file system access
+extension type NodeFs._(JSObject _) implements JSObject {
+  /// `path` string | Buffer | URL | integer filename or file descriptor
+  /// `options` Object | string
+  /// `encoding` string | null Default: null
+  /// `flag` string See support of file system flags. Default: 'r'.
+  /// Returns: string | Buffer
+  ///
+  /// Returns the contents of the path.
+  ///
+  /// If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
+  ///
+  /// Ex to read a file as a string: `fs.readFileSync('/Users/joe/test.txt', 'utf8');`
+  external JSAny readFileSync(JSAny path, JSAny options);
+}
