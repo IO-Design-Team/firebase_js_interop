02df57519f66bbb2334df29949de8864dd1dbd88
Rexios <rexios@rexios.dev>
2025-11-05 22:14:10 -0500

    Fixing example dependency issues

--- example/fji_example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/fji_example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,11 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
-import url_launcher_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
